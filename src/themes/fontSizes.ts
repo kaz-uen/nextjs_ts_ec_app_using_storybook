fd936aaedf2854827310dfc18afd77b8b93d3e54
Kaz Ueno <kazun.ue@gmail.com>
2025-02-11 08:34:41 +0900

    add theme files

---
+++ src/themes/fontSizes.ts
@@ -0,0 +1,13 @@
+/**
+ * プロジェクト内で使用する文字サイズ
+ */
+const fontSizes = {
+  extraSmall: 12,
+  small: 14,
+  medium: 16,
+  mediumLarge: 20,
+  large: 24,
+  extraLarge: 32,
+}
+
+export default fontSizes;
