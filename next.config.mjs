d89f56de99e4dbf3cf1a6f8bb3f8d8b63bb52899
Kaz Ueno <kazun.ue@gmail.com>
2025-02-13 23:13:08 +0900

    add rewrites api proxy on config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,19 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+   //リライト設定（APIのリクエストを別のURLへリダイレクト）
+  async rewrites() {
+    return [
+      {
+        // source（転送元URL）を環境変数 NEXT_PUBLIC_API__BASE_PATH で定義。
+        // :match* はワイルドカード（ex. /api/proxy/*のようなRULをキャッチ）
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:path*`,
+        // destination（転送先URL）を API_BASE_URL に設定
+        // ex. http://localhost:8000 -> /api/proxy/users にリクエストすると http://localhost:8000/users に転送される
+        destination: `${process.env.API_BASE_URL}/:path*`,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
