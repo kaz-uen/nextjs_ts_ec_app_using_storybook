50f00df405ecac2ec14a000884aea4a84d06e511
Kaz Ueno <kazun.ue@gmail.com>
2025-02-26 22:49:53 +0900

    modify home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,40 @@
 import "./reset.css";
 import Layout from "@/components/templates/Layout";
 import Intro from "@/components/organisms/Intro";
+import getAllProducts from "@/services/products/get-all-products";
+import HomePageContainer from "@/containers/HomePageContainer";
+import { ApiContext } from "@/types";
 
-export default function Home() {
+const getProductData = async () => {
+  const context: ApiContext = {
+    apiRootUrl: process.env.API_BASE_URL || 'http://localhost:5000',
+  }
+
+  const [clothesProducts, bookProducts, shoesProducts] = await Promise.all([
+    getAllProducts(context, { category: 'clothes', limit: 6, page: 1}),
+    getAllProducts(context, { category: 'book', limit: 6, page: 1}),
+    getAllProducts(context, { category: 'shoes', limit: 6, page: 1}),
+  ])
+
+  return {
+    clothesProducts,
+    bookProducts,
+    shoesProducts,
+  }
+}
+
+const Home = async () => {
+  const products = await getProductData();
+
   return (
     <Layout>
       <Intro
         title="タイトル"
         description="テキストが入ります。テキストが入ります。テキストが入ります。"
       />
+      <HomePageContainer products={products} />
     </Layout>
   );
 }
+
+export default Home;
---
+++ src/containers/HomePageContainer.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { Product } from "@/types";
+import { theme } from "@/themes";
+import styled from "styled-components";
+import Link from "next/link";
+
+const Root = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const Inner = styled.div`
+  @media screen and (min-width: ${theme.breakPoints.md}) {
+    width: 1040px;
+  }
+`
+
+const Heading = styled.h2`
+  font-size: ${theme.fontSizes.medium}px;
+`
+
+const ProductList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 32px;
+`
+
+const ProductItem = styled.li`
+  display: flex;
+  flex-direction: column;
+`
+
+const ProductImage = styled.img`
+  width: 160px;
+  height: 160px;
+  background-color: gray;
+`
+
+const ProductName = styled.h3`
+  font-size: ${theme.fontSizes.small}px;
+`
+
+const HomePageContainer = ({ products }) => {
+  console.log(products)
+  const {clothesProducts, bookProducts, shoesProducts} = products;
+
+  const renderProductCardCarousel = (products: Product[]) => {
+    return (
+      <ProductList>
+        {products.map((p: Product, i: number) => (
+          <ProductItem key={p.title}>
+            <Link href={`/products/${p.id}`}>
+              <ProductImage src={p.imageUrl} />
+              <ProductName>{p.title}</ProductName>
+              <span>{p.price}円</span>
+            </Link>
+          </ProductItem>
+        ))}
+      </ProductList>
+    )
+  }
+
+  return (
+    <Root>
+      <Inner>
+        <Heading>トップス</Heading>
+        {renderProductCardCarousel(clothesProducts)}
+        <hr />
+        <Heading>本</Heading>
+        {renderProductCardCarousel(bookProducts)}
+        <hr />
+        <Heading>シューズ</Heading>
+        {renderProductCardCarousel(shoesProducts)}
+      </Inner>
+    </Root>
+  );
+}
+
+export default HomePageContainer;
